ab95a3826c96e79a5368914d9f481308f8cfa8c0
hanmacrad <37249407+hanmacrad@users.noreply.github.com>
2020-06-04 17:23:24 +0100

    Create 1_Simulation.R

---
+++ simulation/1_Simulation.R
@@ -0,0 +1,35 @@
+#Simulate Branching Process 
+
+#Parameters
+r0 = 1.1
+num_days = 30
+
+#Function
+simulate_branching = function(num_days, r0){
+  
+  #Set up
+  vec_num_infecteds = vector('numeric', num_days)
+  vec_num_infecteds[1] = 1
+  
+  for (t in 2: num_days){
+    #print(t)
+    cat("time t: ", t)
+    num_new_infecteds = 0
+    for (i in 1: vec_num_infecteds[t-1]){
+      cat("individual i: ", i)
+      num_new_infecteds = num_new_infecteds + rpois(1, r0)
+      cat("num_new_infecteds: ", num_new_infecteds)
+    }
+    vec_num_infecteds[t] = num_new_infecteds
+  }
+  
+  vec_num_infecteds
+}
+
+#Implement
+x = simulate_branching(30, r0)
+
+#Plots
+plot.ts(x)
+cum_data <-cumsum(vec_num_infecteds)
+plot.ts(cum_data)
