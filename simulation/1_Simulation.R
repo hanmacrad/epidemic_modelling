3d8dd16ea9fe99603b067f7af487cade3ceab527
hanmacrad <37249407+hanmacrad@users.noreply.github.com>
2020-06-08 14:34:39 +0100

    Removed excessive for loops, still in progress

--- simulation/1_Simulation.R
+++ simulation/1_Simulation.R
@@ -2,7 +2,7 @@
 
 #Parameters
 num_days = 30
-r0 = 2.2
+r0 = 2.1
 shape_gamma = 6
 scale_gamma = 1
 
@@ -13,32 +13,29 @@ simulate_branching = function(num_days, r0, shape_gamma, scale_gamma) {
   vec_infecteds = c() #vector('numeric', num_days)
   vec_infecteds[1] = 1
   
+  #Infectiousness (Discrete gamma)
+  prob_infect = pgamma(c(1:num_days), shape = shape_gamma, scale = scale_gamma) - pgamma(c(0:(num_days-1)), shape = shape_gamma, scale = scale_gamma)
+  
   for (t in 2:num_days) {
-    num_new_infecteds = 0
-    for (t_inf in 1:length(vec_infecteds)) { #Loop through each past time index
-      
-      for (i in 1:vec_infecteds[t_inf]) { #Get infectiousness of each individual at each tinf
-        
-        #Infectiousness (Discrete gamma)
-        ti2 = t - t_inf
-        ti1 = ti2 - 1
-        prob_infect = pgamma(ti2, shape = shape_gamma, scale = scale_gamma) - pgamma(ti1, shape = shape_gamma, scale = scale_gamma)
-        
-        num_new_infecteds = num_new_infecteds + rpois(1, r0 * prob_infect)
-      }
-    }
-    vec_infecteds[t] = num_new_infecteds
+    
+    #New infections
+    vec_infecteds[t] = sum(rpois(sum(vec_infecteds), r0*sum(prob_infect[1:t])))
   }
   
   vec_infecteds
 }
 
 #Implement
+start_time = Sys.time()
 x = simulate_branching(num_days, r0, shape_gamma, scale_gamma)
+end_time = Sys.time()
+time_elap = end_time - start_time
+#print(time_elap)
+x
 
 #Plots
-plot.ts(x, ylab = "Num Daily infections")
-cum_data <- cumsum(vec_infecteds)
+plot.ts(x, ylab = "N Daily infections")
+cum_data <- cumsum(x)
 plot.ts(cum_data)
 
 
@@ -48,4 +45,5 @@ a = seq(0.0, 10, by = 1)
 b = pgamma(a, shape = 6, scale = 1)
 plot(a, b)
 
-pgamma(8, shape = 6, scale = 1) - pgamma(6, shape = 6, scale = 1)
+
+
