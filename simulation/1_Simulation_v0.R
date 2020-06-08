c433ea32c72188db47c263d2c88e0f81c23b4dc4
hanmacrad <37249407+hanmacrad@users.noreply.github.com>
2020-06-08 14:33:39 +0100

    Drafts

--- simulation/1_Simulation_v0.R
+++ simulation/1_Simulation_v0.R
@@ -82,3 +82,66 @@ b = pgamma(a, shape = 6, scale = 1)
 plot(a, b)
 
 pgamma(2, shape = 6, scale = 1)
+
+#Version 0.2
+#Function
+simulate_branching = function(num_days, r0, shape_gamma, scale_gamma) {
+  #Set up
+  vec_infecteds = c() #vector('numeric', num_days)
+  vec_infecteds[1] = 1
+  
+  for (t in 2:num_days) {
+    num_new_infecteds = 0
+    
+    for (t_inf in 1:length(vec_infecteds)) { #Loop through each past time index
+      
+      for (i in 1:vec_infecteds[t_inf]) { #Get infectiousness of each individual at each tinf
+        
+        #Infectiousness (Discrete gamma)
+        ti2 = t - t_inf
+        ti1 = ti2 - 1
+        prob_infect = pgamma(ti2, shape = shape_gamma, scale = scale_gamma) - pgamma(ti1, shape = shape_gamma, scale = scale_gamma)
+        
+        num_new_infecteds = num_new_infecteds + rpois(1, r0 * prob_infect)
+      }
+    }
+    vec_infecteds[t] = num_new_infecteds
+  }
+  
+  vec_infecteds
+}
+
+#Implement
+x = simulate_branching(num_days, r0, shape_gamma, scale_gamma)
+
+#Version 0.3
+simulate_branching = function(num_days, r0, shape_gamma, scale_gamma) {
+  #Set up
+  vec_infecteds = c() #vector('numeric', num_days)
+  vec_infecteds[1] = 1
+  
+  for (t in 2:num_days) {
+    
+    #num_new_infecteds = 0
+    t_inf = 1:length(vec_infecteds)
+    
+    #Infectiousness (Discrete gamma)
+    ti2 = t - t_inf #Vectorwise 
+    ti1 = ti2 - 1
+    prob_infect = pgamma(ti2, shape = shape_gamma, scale = scale_gamma) - pgamma(ti1, shape = shape_gamma, scale = scale_gamma)
+    #print(prob_infect)
+    
+    #New infections (Poisson)
+    #vec_new_infecteds = rpois(vec_infecteds, r0 * prob_infect)
+    print()
+    #avec_infecteds[t] = length(rpois(vec_infecteds, r0 * prob_infect))
+  }
+  
+  vec_infecteds
+}
+
+#Vector indexing
+for(j in (n:L)){
+  sel <- (j-n+1):j
+  signal[j] <- sum(abs(V_s[sel] - V_b[sel])) / (n*V)
+}
