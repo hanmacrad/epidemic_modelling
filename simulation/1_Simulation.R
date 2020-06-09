748d39114e7a42a5846b7a92ac2989190abd33da
hanmacrad <37249407+hanmacrad@users.noreply.github.com>
2020-06-09 21:44:42 +0100

    Plots to inspect varying r0, k, theta

--- simulation/1_Simulation.R
+++ simulation/1_Simulation.R
@@ -1,8 +1,9 @@
 #Simulate Branching Process
+par(mar=c(1,1,1,1))
 
 #Parameters
-num_days = 20
-r0 = 2.2
+num_days = 50
+r0 = 4.2
 shape_gamma = 6
 scale_gamma = 1
 
@@ -39,12 +40,31 @@ plot.ts(x, ylab = "N Daily infections")
 cum_data <- cumsum(x)
 plot.ts(cum_data)
 
-
-#Rough work
 #Inspect gamma density
 a = seq(0.0, 10, by = 1)
-b = pgamma(a, shape = 6, scale = 1)
+b = dgamma(a, shape = 1.5, scale = 2)
 plot(a, b)
 
+#*********************************************************************
+#Plots
+plot_variations = function(list_r0, list_scale_shape, num_days){
+  #Plot
+  par(mfrow=c(3,5))
+  for (shape_scale in list_scale_shape){
+    shape_gamma = shape_scale[[1]]
+    scale_gamma = shape_scale[[2]]
+    for (r0 in list_r0){
+      num_daily = simulate_branching(num_days, r0, shape_gamma, scale_gamma)
+      plot.ts(num_daily,lwd = 2, ylab = "N Daily infections", col = 'purple')
+    }
+  }
+}
 
+#*********
+#Params + Apply
+num_days = 30
+list_r0 = c(0.5, 1, 2, 4, 8)
+#list_shape_scale = list(list(9,0.5) , list(7.5,1.0), list(6,1))
+list_shape_scale = list(list(2,2), list(3,2), list(1.5, 2))
+plot_variations(list_r0,  list_shape_scale, num_days)
 
