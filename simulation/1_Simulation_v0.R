9e07b10cba2e555b249a4657e6792f99b31bdcb5
hanmacrad <37249407+hanmacrad@users.noreply.github.com>
2020-06-09 21:44:17 +0100

    drafts

--- simulation/1_Simulation_v0.R
+++ simulation/1_Simulation_v0.R
@@ -145,3 +145,54 @@ for(j in (n:L)){
   sel <- (j-n+1):j
   signal[j] <- sum(abs(V_s[sel] - V_b[sel])) / (n*V)
 }
+
+#***** Rough ***********************
+#Apply 
+matrix_shape_scale = rbind(c(1.5,2),c(2,2), c(3,2), c(5,1), c(9,0.5), c(7.5, 1.0))
+matrix_shape_scale
+
+par(mfrow=c(3,5))
+for (r in 1:nrow(matrix_shape_scale)){
+  for (c in 1:ncol(matrix_shape_scale)){
+    print(matrix_shape_scale[r,c])
+    
+    for (i_r0 in 1:length(list_r0)){
+      
+      n_daily = simulate_branching(num_days, i_r0, shape_gamma, scale_gamma)
+      
+    }
+    
+  }
+  print('*')
+}
+
+N = 100000
+list_sigma1 = c(10, 100) #c(1, 2, 5) #c(0.001, 0.01, 0.1) #c(1, 2, 5) # #c(1, 10, 100) 
+rate_acc = results_plot_ts_rate_acc(list_sigma1, list_alpha, N)
+
+
+#*******************
+#List of lists
+list_shape_scale = list(list(1.5, 2), list(2,2), list(3,2), list(5,1), list(9,0.5), list(7.5,1.0))
+
+
+list1 <- list(2, 3)
+list2 <- list(7, 1)
+mylist <- list(list1, list2)
+
+for (listx in mylist) {
+  print(typeof(listx[[1]]))
+  print(listx[[2]])
+  print('*')
+}
+
+
+
+#####
+#Rough work
+# Create a matrix
+mat <- matrix(data = seq(10, 20, by=1), nrow = 6, ncol =2)
+# Create the loop with r and c to iterate over the matrix
+for (r in 1:nrow(mat))   
+  for (c in 1:ncol(mat))  
+    print(paste("Row", r, "and column",c, "have values of", mat[r,c])) 
