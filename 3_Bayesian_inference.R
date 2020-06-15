29652a6597b6ce04ee35e5c5876e1bb93d2d9648
hanmacrad <37249407+hanmacrad@users.noreply.github.com>
2020-06-15 12:19:17 +0100

    Bayesian model using JAGS in progress

--- 3_Bayesian_inference.R
+++ 3_Bayesian_inference.R
@@ -20,11 +20,55 @@ scale_gamma = 1
 #Data
 y = simulate_branching(num_days, r0, shape_gamma, scale_gamma)
   
+
+#Model
+cat("model
+    {
+
+  #Priors
+  r0~exp(1)
+
+  #Likelihood
+  #Params
+  num_days = length(y)
+
+  #Infectiousness (Discrete gamma)
+  prob_infect = pgamma(c(1:num_days), shape = shape_gamma, scale = scale_gamma) - pgamma(c(0:(num_days-1)), shape = shape_gamma, scale = scale_gamma)
+  likel = 1
+
+  for (t in 2:num_days) {
+
+    lambda = r0*sum(y[1:t-1]*rev(prob_infect[1:t-1]))
+    likel = likel*lambda^(y[t])*exp(-lambda)
+    
+    }
+
+  }",file = "bayesian_model.txt")
+
+#Initial values
+inits <- NULL
+
+#Parameters to track
+params = c('r0')
+
+#Hyperparameters
+ni = 10000 # number of iterations
+nb = 1000 # burn in interval
+nt = 1 # thinning interval
+nc = 3 # number of chains
+
+#Compile model
+jmod = jags.model(file = 'bayesian_model.txt', data = y, n.chains = nc, inits = inits, n.adapt = 1000)
+
+
+
+#********
+#Drafts
 #Model
 model_bayesian = function(){
   
   #Priors
-  r0 ~ exp(1)
+  r0~exp(1)
   
   #Likelihood
   #Params
@@ -44,25 +88,5 @@ model_bayesian = function(){
 }
 
 #Write Model
-model.file = "model.file"
-write.model(model_bayesian, model.file)
-
-#Initial values
-inits <- NULL
-
-#Parameters to track
-params = c('r0')
-
-#Hyperparameters
-# number of iterations
-ni = 10000
-# burn in interval
-nb = 1000
-# thinning interval
-nt = 1
-# number of chains
-nc = 3
-
-# compile model
-jmod = jags.model(file = model.file, data = y, n.chains = nc, inits = inits, n.adapt = 1000)
-
+#model.file = "model.file"
+#write.model(model_bayesian, model.file)
