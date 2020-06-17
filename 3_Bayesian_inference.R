df3fca5265901b489dca9fceae5ee75da61599cd
hanmacrad <37249407+hanmacrad@users.noreply.github.com>
2020-06-17 13:13:53 +0100

    Plotting posterior in progress

--- 3_Bayesian_inference.R
+++ 3_Bayesian_inference.R
@@ -1,92 +1,50 @@
-##To do
-#1. Download JAGs
-#2. Inspect results
-
-#Packages
-library(R2OpenBUGS)
-library(rjags)
-library(coda)
-library(MCMCvis)
-
 source("1_simulation.R")
 source("3_bayesian_inference.R")
 
-#Params
-r0 = 3.5
-num_days = 45
+#Data Params
+r0 = 4.0
+num_days = 20
 shape_gamma = 6
 scale_gamma = 1
 
 #Data
-y = simulate_branching(num_days, r0, shape_gamma, scale_gamma)
+x = simulate_branching(num_days, r0, shape_gamma, scale_gamma)
+
+#Priors
+prior_r0_k = 1
+prior_r0_theta = 1
+
+#Posterior
+plot_posterior <- function(x, prior_r0_k, prior_r0_theta){
   
-
-#Model
-cat("model
-    {
-
-  #Priors
-  r0~exp(1)
-
-  #Likelihood
   #Params
   num_days = length(y)
-
-  #Infectiousness (Discrete gamma)
-  prob_infect = pgamma(c(1:num_days), shape = shape_gamma, scale = scale_gamma) - pgamma(c(0:(num_days-1)), shape = shape_gamma, scale = scale_gamma)
-  likel = 1
-
-  for (t in 2:num_days) {
-
-    lambda = r0*sum(y[1:t-1]*rev(prob_infect[1:t-1]))
-    likel = likel*lambda^(y[t])*exp(-lambda)
-    
-    }
-
-  }",file = "bayesian_model.txt")
-
-#Initial values
-inits <- NULL
-
-#Parameters to track
-params = c('r0')
-
-#Hyperparameters
-ni = 10000 # number of iterations
-nb = 1000 # burn in interval
-nt = 1 # thinning interval
-nc = 3 # number of chains
-
-#Compile model
-jmod = jags.model(file = 'bayesian_model.txt', data = y, n.chains = nc, inits = inits, n.adapt = 1000)
-
-
-
-#********
-#Drafts
-#Model
-model_bayesian = function(){
-  
-  #Priors
-  r0~exp(1)
-  
-  #Likelihood
-  #Params
-  num_days = length(y)
+  shape_gamma = 6
+  scale_gamma = 1
   
   #Infectiousness (Discrete gamma)
   prob_infect = pgamma(c(1:num_days), shape = shape_gamma, scale = scale_gamma) - pgamma(c(0:(num_days-1)), shape = shape_gamma, scale = scale_gamma)
-  likel = 1
+  sum_lambda = 0
   
   for (t in 2:num_days) {
     
-    lambda = r0*sum(y[1:t-1]*rev(prob_infect[1:t-1]))
-    likel = likel*lambda^(y[t])*exp(-lambda)
+    lambda_t = sum(x[1:t-1]*rev(prob_infect[1:t-1]))
+    sum_lambda = sum_lambda + lambda_t
     
   }
   
+  #Posterior
+  sum_x = sum(x)
+  vec_r0 = seq(from=0.1, to = 20, by = 0.1)
+  post_gamma_shape = sum_x + prior_r0_k
+  post_gamma_scale = 1/(sum_lambda + (1/prior_r0_theta))
+  density_gamma = dgamma(vec_r0, shape = post_gamma_shape, scale = post_gamma_scale)
+  
+  #Plot
+  plot(vec_r0, density_gamma)
+  
 }
 
-#Write Model
-#model.file = "model.file"
-#write.model(model_bayesian, model.file)
+#Apply
+plot_posterior(x, prior_r0_k, prior_r0_theta)
+
